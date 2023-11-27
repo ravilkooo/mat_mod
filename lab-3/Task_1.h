e2c52f6d42ce485c02013a2e41ac5e23c4fb3457
ravil <ravilka.zftsh@yandex.ru>
2023-11-27 21:59:24 +0500

    task4_method

--- lab-3/Task_1.h
+++ lab-3/Task_1.h
@@ -1,14 +1,27 @@
 #include <vector>
 
-void do_experiment(long double alpha_in, long double beta_in, long double k, long double algo_type, std::string name="");
+void do_experiment(long double alpha_in, long double beta_in,
+    long double k, long double algo_type,
+    std::vector<long long> times,
+    std::string name,
+    bool console_log);
 long double FPU_der(long double r);
+long double FPU(long double r);
 void verle(std::vector<long double>& q_cur,
-           std::vector<long double>& v_cur,
-           long long k,
-           long long n,
-           long double dt);
+    std::vector<long double>& v_cur,
+    long long k,
+    long long n,
+    long double dt,
+    std::vector<long long> times,
+    std::vector<std::vector<long double>>& checkpoints,
+    std::vector<long double>& energy,
+    bool console_log);
 void simpl_verle(std::vector<long double>& q_cur,
-           std::vector<long double>& v_cur,
-           long long k,
-           long long n,
-           long double dt);
+    std::vector<long double>& v_cur,
+    long long k,
+    long long n,
+    long double dt,
+    std::vector<long long> times,
+    std::vector<std::vector<long double>>& checkpoints,
+    std::vector<long double>& energy,
+    bool console_log);
